c2709ff851e8d097c26db32012dd0d054751abaa
stjohnsenekal <john.senekal@gmail.com>
2021-03-10 08:32:04 +0200

    babel and webpack.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  output: {
+    filename: 'sailthru.main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  watch: true
+};
